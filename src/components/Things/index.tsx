992b81d7497fe2fbba826d6524a7b72633cac2cf
Jayphen <jayphen@hyperlab.se>
2019-06-19 18:55:43 +0200

    Update some deps

--- src/components/Things/index.tsx
+++ src/components/Things/index.tsx
@@ -2,6 +2,35 @@ import React from "react";
 import { useQuery } from "react-apollo-hooks";
 import { getAllThings } from "../../queryTypes/getAllThings";
 import thingsQuery from "./thingsQuery.gql";
+import styled from "@emotion/styled";
+import { maxWidth } from "../ui/MaxWidth";
+import { Link } from "react-router-dom";
+
+const ThingsContainer = styled("div")`
+  ${maxWidth};
+  display: grid;
+  grid-gap: 1rem;
+  a {
+    text-decoration: none;
+  }
+  h1 {
+    font-size: 1rem;
+    color: black;
+  }
+  aside {
+    font-size: 0.75em;
+    text-transform: uppercase;
+    font-weight: 500;
+    color: hsl(0, 0%, 60%);
+  }
+`;
+const Thing = styled("div")`
+  background: white;
+  padding: 1em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default function Things() {
   const { data, error, loading } = useQuery<getAllThings>(thingsQuery);
@@ -12,9 +41,19 @@ export default function Things() {
   const things = data.things || [];
 
   return things ? (
-    <div>
-      {things.map(thing => thing && <div key={thing.id}>{thing.name}</div>)}
-    </div>
+    <ThingsContainer>
+      {things.map(
+        thing =>
+          thing && (
+            <Link key={thing.id} to={`/place/${thing.place.id}`}>
+              <Thing>
+                <h1>{thing.name}</h1>
+                <aside>{thing.place.name}</aside>
+              </Thing>
+            </Link>
+          ),
+      )}
+    </ThingsContainer>
   ) : (
     <div>no things</div>
   );
--- src/queryTypes/getAllThings.ts
+++ src/queryTypes/getAllThings.ts
@@ -6,10 +6,17 @@
 // GraphQL query operation: getAllThings
 // ====================================================
 
+export interface getAllThings_things_place {
+  __typename: "Place";
+  id: string;
+  name: string;
+}
+
 export interface getAllThings_things {
   __typename: "Thing";
   id: string;
   name: string;
+  place: getAllThings_things_place;
 }
 
 export interface getAllThings {
